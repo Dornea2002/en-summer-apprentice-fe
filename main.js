819430721953216bb6f2f3925639367db828a521
Dornea2002 <104447231+Dornea2002@users.noreply.github.com>
2023-08-10 13:23:55 +0300

    first push not working

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-// Imports
-import { addEvents, handleSearch } from './src/utils';
-import { createOrderItem } from './src/components/createOrderItem.js';
-import { getTicketCategories } from './src/components/api/getTicketCategories.js';
-import { removeLoader, addLoader } from './src/components/loader';
-import './src/mocks/handlers';
-
-let events = null;
+import { create } from "domain";
+import {usesStyles} from './src/components/styles';
+import { addPurchase, kebabCase } from './src/utils';
 
 // Navigate to a specific URL
 function navigateTo(url) {
@@ -13,94 +8,37 @@ function navigateTo(url) {
   renderContent(url);
 }
 // HTML templates
+// function getHomePageTemplate() {
+//   return `
+//    <div id="content" >
+//       <img src="./src/assets/bannerHomePage.jpeg" alt="summer">
+//       <div class="events flex items-center justify-center flex-wrap">
+//       </div>
+//     </div>
+//   `;
+// }
+
+
 function getHomePageTemplate() {
   return `
-   <div id="content" class="hidden">
-      <img src="./src/assets/Endava.png" alt="summer">
-      <div class="flex flex-col items-center">
-        <div class="w-80">
-          <h1>Explore Events</h1>
-          <div class="filters flex flex-col">
-            <input type="text" id="filter-name" placeholder="Filter by name" class="px-4 mt-4 mb-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500" />
-            <button id="filter-button" class="px-4 py-2 text-white filter-btn rounded-lg">Filter</button>
-          </div>
-        </div>
+   <div id="content" style="display: flex; flex-direction: column; align-items: center; position: relative;">
+      <img src="./src/assets/bannerHomePage.jpeg" alt="summer" style="width: 100%; height: auto;">
+      <div class="title-overlay" style="position: absolute; top: 10%; left: 50%; transform: translate(-50%, -50%); padding: 10px;">
+        <h1 style="margin: 0; text-align: center; color: white;">Ticket Sales Management</h1>
       </div>
       <div class="events flex items-center justify-center flex-wrap">
       </div>
-      <div class="cart"></div>
     </div>
   `;
 }
 
+
 function getOrdersPageTemplate() {
   return `
-      <div id="content" class="hidden">
-        <h1 class="text-2xl mb-4 mt-8 text-center">Purchased Tickets</h1>
-        <div class="purchases ml-6 mr-6">
-          <div class="bg-white px-4 py-3 gap-x-4 flex font-bold">
-            <span class="flex-1">Name</span>
-            <span class="flex-1 flex justify-end">Nr tickets</span>
-            <span class="flex-1">Category</span>
-            <span class="flex-1 hidden md:flex">Date</span>
-            <span class="w-12 text-center hidden md:flex">Price</span>
-            <span class="w-28 sm:w-8"></span>
-          </div>
-        </div>
-      </div>
+    <div id="content">
+    <h1 class="text-2xl mb-4 mt-8 text-center">Purchased Tickets</h1>
+    </div>
   `;
-}
-
-function liveSearch() {
-  const filterInput = document.querySelector('#filter-name');
-
-  if(filterInput) {
-    const searchValue = filterInput.value;
-    
-    if(searchValue) {
-      const filteredEvents = events.filter(event => event.name.toLowerCase().includes(searchValue.toLowerCase()));
-    
-      addEvents(filteredEvents);
-    }
-  }
-
-}
-
-function setupFilterEvents() {
-  const nameFilterInput = document.querySelector('#filter-name');
-
-  if(nameFilterInput) {
-    const filterInterval = 500;
-
-    nameFilterInput.addEventListener('keyup', () => {
-      setTimeout(liveSearch, filterInterval);
-    });
-  }
-}
-
-function setupSearchEvents() {
-  const searchForm = document.querySelector('.search-form');
-  const searchInput = document.querySelector('.search-input');
-  const searchButton = document.querySelector('.search-button');
-  const eventSection = document.querySelector('.events');
-
-  
-  if(searchForm) {
-    searchForm.addEventListener('submit', async (e) => {
-      e.preventDefault();
-      const searchTerm = searchInput.value.trim().toLowerCase();
-      const resultsFound = await handleSearch(searchTerm);
-      if (!resultsFound) {
-        eventSection.innerHTML = 'No results found';
-      }
-    });
-  }
-
-  if(searchButton) {
-    searchButton.addEventListener('click', () => {
-      searchInput.classList.toggle('active');
-    });
-  }
 }
 
 function setupNavigationEvents() {
@@ -137,59 +75,231 @@ function setupInitialPage() {
   renderContent(initialUrl);
 }
 
-async function fetchTicketEvents() {
-  const response = await fetch('/api/ticketEvents');
+
+function renderHomePage(){
+  const mainContentDiv=document.querySelector('.main-content-component');
+  mainContentDiv.innerHTML=getHomePageTemplate();
+
+  console.log('function', fetchTicketEvents());
+  fetchTicketEvents().then(data => {
+    // console.log('data', data);
+    addEvents(data);
+  })
+}
+
+async function fetchTicketEvents(){
+  const response = await fetch('http://localhost:8080/event');
+  // console.log(response);
+
   const data = await response.json();
+
+  // console.log('data', data);
+
   return data;
 }
 
-async function fetchOrders() {
-  const response = await fetch('/api/orders');
-  const orders = await response.json();
-  return orders;
-}
 
-function renderHomePage() {
-  const mainContentDiv = document.querySelector('.main-content-component');
-  mainContentDiv.innerHTML = getHomePageTemplate();
-
-  setupFilterEvents();
-  setupSearchEvents();
-  addLoader();
-
-  fetchTicketEvents()
-    .then((data) => {
-      events = data;
-      setTimeout(() => {
-        removeLoader();
-      }, 200);
-      addEvents(events);
+const addEvents = (events) => {
+  const eventsDiv=document.querySelector('.events');  //biggest container containing all events
+  eventsDiv.innerHTML = 'No invents';
+  if(events.length){
+    eventsDiv.innerHTML = '';
+    events.array.forEach(event => {
+      eventsDiv.appendChild(createEvent(event)); //introducing event in the container to be displayed
     });
-}
+  }
+
+};
+
+const createEvent = (eventData) => {
+  const title = kebabCase(eventData.eventType.name);
+  const eventElement = createEventElement(eventData, title);
+
+  return eventElement;
+}; 
+
+const createEventElement = (eventData, title) => {
+  const {id, eventDescription, img, eventName, ticketCategory, startDate, endDate, venueDTO} = eventData;
+    const eventDiv = document.createElement('div');
+    const eventWrapperClasses = usesStyles('eventWrapper');
+    const actionsWrapperClasses = usesStyles('actionsWrapper');
+    const quantityClasses = usesStyles('quantity');
+    const inputClasses = usesStyles('input');
+    const quantityActionClasses = usesStyles('quantityAction');
+    const increaseBtnClasses = usesStyles('increaseBtn');
+    const decreaseBtnClasses = usesStyles('decreaseBtn');
+    const addToCartBtnClasses = usesStyles('addToCartBtn');
+
+    // Set up event wrapper
+    eventDiv.classList.add(...eventWrapperClasses);
+
+  // Format LocalDateTime values to DD/MM/YYYY
+  const formattedStartDate = new Date(startDate).toLocaleDateString('en-GB');
+  const formattedEndDate = new Date(endDate).toLocaleDateString('en-GB');
+
+    // Create the event content markup
+    const contentMarkup = `
+    <header>
+        <h2 class="event-title text=2xl font-bold">${eventName}</h2>
+    </header>
+    <div class="content">
+      <img src="${img}" class="event-image w-full height-200 rounded"
+      <p class="description text-gray-700 text-center text-sm">${eventDescription}</p>
+      <p class="date text-gray-700 text-sm text-center">Date: ${formattedStartDate} - ${formattedEndDate}</p>
+      <p class="venue text-gray-700 text-center text-sm">Location: ${venueDTO.location}</p>
+    </div>
+    `;
+  eventDiv.innerHTML = contentMarkup;
+
+  // create ticket type selection and quantity input
+  const actions = document.createElement('div');
+  actions.classList.add(...actionsWrapperClasses);
+
+  const categoriesOptions = ticketCategory.map((category) =>
+  `<option value=${category.ticketCategoryID}>${category.description}</option>`);
+
+  const ticketSection = createSectionWithIcon('./src/assets/icons/ticket.png', `
+  <select id="ticketType" name="ticketType" class="select ${title}-ticket-type text-sm bg-white border border-gray-300 rounded px-2 py-1 ">
+    ${categoriesOptions.join('\n')}
+  </select>
+
+`);
+
+  const ticketTypeMarkup = `
+  <h2 class="text-lg font-bold mb-2">Choose Ticket Type:</h2>
+  <select id="ticketType" name="ticketType" class="select ${title}-ticket-type border border
+    ${categoriesOptions.join('/n')}
+    </select>
+  `;
+    console.log(categoriesOptions);
+
+    actions.innerHTML = ticketTypeMarkup;
+
+    const quantity = document.createElement('div');
+    quantity.classList.add(...quantityClasses);
+
+    const input = document.createElement('input');
+    input.classList.add(...inputClasses);
+    input.type = 'number';
+    input.min = '0';
+    input.value = '0';
+
+    input.addEventListener('blur', () => 
+    { 
+      if(!input.value){
+        input.value = 0;
+      }
+    });
+
+    input.addEventListener('input', () => 
+    { 
+      const currentQuantity = parseInt(input.value);
+      if(currentQuantity > 0){
+        addToCart.disabled=false;
+      }
+      else{
+        addToCart.disabled=true;
+      }
+    });
+
+    quantity.appendChild(input);
+
+    const quantityActions = document.createElement('div');
+    quantityActions.classList.add(...quantityActionClasses);
+
+    const increase = document.createElement('button');
+    increase.classList.add(...increaseBtnClasses);
+    increase.innerText = '+';
+    increase.addEventListener('click', () =>
+    {
+        input.value = parseInt(input.value);
+        if(currentQuantity > 0){
+          addToCart.disabled = false;
+        }
+        else {
+          addToCart.disabled = true;
+        }
+    } 
+    );
+
+    const decrease = document.createElement('button');
+    decrease.classList.add(...decreaseBtnClasses);
+    decrease.innerText = '-';
+    decrease.addEventListener('click', () => {
+      const currentValue = parseInt(input.value);
+      if(currentValue > 0){
+        input.value = currentValue -1;
+      }
+
+      const currentQuantity = parseInt(input.value);
+      if(currentQuantity > 0){
+        addToCart.disabled = false;
+      }
+      else{
+        addToCart.disabled = true;
+      }
+    });
+
+    quantityActions.appendChild(increase);
+    quantityActions.appendChild(decrease);
+
+    quantity.appendChild(quantityActions);
+    actions.appendChild(quantity);
+    eventDiv.appendChild(actions);
+
+    //Create the event footer with "Add to cart" button
+    const eventFooter = document.createElement('footer');
+    const addToCart = document.createElement('button');
+    addToCart.classList.add(...addToCartBtnClasses);
+    addToCart.innerText = 'Add To Cart';
+    addToCart.disabled = true;
+
+    addToCart.addEventListener('click', ()=>{
+      handleAddToCart(title, id, input, addToCart);
+    });
+
+    eventFooter.appendChild(addToCart);
+    eventDiv.appendChild(eventFooter);
+
+    return eventDiv;
+};
+
+const handleAddToCart = (title, id, input, addToCart) => {
+  const ticketType = document.querySelector(`.${kebabCase(title)}-ticket-type`).value;
+  const quantity = input.value;
+  if(parseInt(quantity)){
+    const customerID=1;
+    fetch(`http://localhost:8080/orders/findBy/${userId}`, {
+      methot:"POST",
+      headers:{
+        "Content-Type":"application/json",
+      },
+      body:JSON.stringify({
+        ticketType:+ticketType,
+        eventID: id,
+        quantity:+quantity,
+      })
+    }).then((response)=> {
+      return response.json().then((data)=>{
+        if(!response.ok){
+          console.log("Something went wrong...");
+        }
+        return data;
+      })
+    }).then((data) =>{
+      addPurchase(data);
+      console.log("Done!");
+      input.value = 0;
+      addToCart.disabled = true;
+    });
+  }else{
+    //Not integer. To be treated
+  }
+};
 
 function renderOrdersPage(categories) {
   const mainContentDiv = document.querySelector('.main-content-component');
   mainContentDiv.innerHTML = getOrdersPageTemplate();
-
-  const purchasesDiv = document.querySelector('.purchases');
-  addLoader();
-
-  if (purchasesDiv) {
-    fetchOrders()
-      .then((orders) => {
-        if (orders.length > 0) {
-          setTimeout(() => {
-            removeLoader();
-          }, 200);
-          orders.forEach((order) => {
-            const newOrder = createOrderItem(categories, order);
-            purchasesDiv.appendChild(newOrder);
-          });
-        } else {
-          removeLoader();
-        }
-      });
-  }
 }
 
 // Render content based on URL
@@ -200,20 +310,11 @@ function renderContent(url) {
   if (url === '/') {
     renderHomePage();
   } else if (url === '/orders') {
-    getTicketCategories()
-      .then((categories) => {
-        renderOrdersPage(categories);
-      })
-      .catch((error) => {
-        console.error('Error fetching ticket categories:', error);
-      });
+    renderOrdersPage()
   }
 }
 
-
 // Call the setup functions
-setupFilterEvents();
-setupSearchEvents();
 setupNavigationEvents();
 setupMobileMenuEvent();
 setupPopstateEvent();
